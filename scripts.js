7ba9e43b8abf351bba2f074aa8b2f0aea5d048b5
BrandonW52 <157122603+BrandonW52@users.noreply.github.com>
2024-07-10 13:06:12 +0200

    Resolve user stories 3 && 4

--- scripts.js
+++ scripts.js
@@ -4,6 +4,14 @@ const result = document.querySelector("[data-result]");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  if (typeof entries !== "number" || entries == "null") {
+    result.innerText =
+      "Division not performed. Both values are required in inputs. Try again";
+  } else {
+    const { dividend, divider } = Object.fromEntries(entries);
+
+    // Added rounds down to the first whole number
+    result.innerText = Math.floor(dividend / divider);
+  }
 });
